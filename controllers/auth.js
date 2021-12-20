73892d57cf9221f1e67ae2d0cf20edc5d2233183
davidomole <thedavidomole@gmail.com>
2021-12-20 19:31:48 +0100

    complete signup method

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,13 +9,20 @@ const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
 
 const login = () => {};
-const signup = (req, res) => {
+const signup = async(req, res) => {
   try {
       const {fullName, userName, password, phoneNumber} = req.body;
 
       const userId = crypto.randomBytes(16).toString('hex');
 
       const serverClient = connect(api_key, api_secret, app_id);
+
+      const hashedPassword = await bcrypt.hash(password,10);
+
+      const token =serverClient.createUserToken(userId);
+
+      res.status(200).json({token, fullName, userName, userId, hashedPassword, phoneNumber})
+
   } catch (error) {
     res.status(500).json({ message: error });
   }
