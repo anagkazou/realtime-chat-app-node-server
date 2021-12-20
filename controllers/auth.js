60db1d0daf837596d60a0338a46633cb982995e7
davidomole <thedavidomole@gmail.com>
2021-12-20 20:46:16 +0100

    complete login function

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,30 @@ const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const app_id = process.env.STREAM_APP_ID;
 
-const login = () => {};
+const login = async (req,res) => {
+    try {
+        const {userName, password} =req.body;
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key,api_secret)
+
+        const {users} = await client.queryUsers({name: userName})
+
+        if(users.length === 0 ) res.status(400).json({message: 'User not found'})
+
+        const success = await bcrypt.compare(password, users[0].hashedPassword);
+        const token = serverClient.createUserToken(users[0].id)
+
+        if (success){
+            res.status(200).json({token, fullName:users[0].fullName, userName, userId:users[0].id})
+        }else{
+            res.status(500).json({message:'Incorrect password'});
+        }
+    } catch (error) {
+        res.status(500).json({message:error});
+
+        
+    }
+};
 const signup = async(req, res) => {
   try {
       const {fullName, userName, password, phoneNumber} = req.body;
